ebe9b31290c51005bcdc7bb0c523e4a18dbf9fd3
Kareem-izzat <97916953+Kareem-izzat@users.noreply.github.com>
2025-09-22 22:24:58 +0300

    initial files

---
+++ learn-it/src/test/java/com/example/learn_it/LearnItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
